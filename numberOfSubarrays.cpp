0697355fe286b2b13d46422d2b043aa56f00619d
Vishal Kurve <vishalkurve7@gmail.com>
2024-06-22 20:52:06 +0530

    Update numberOfSubarrays.cpp

--- numberOfSubarrays.cpp
+++ numberOfSubarrays.cpp
@@ -21,3 +21,38 @@ int numberOfSubarrays(vector<int>& nums, int k) {
         }
         return ans;
     }
+
+// using sliding window
+
+int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int oddCount = 0;
+        int count    = 0;
+        int result   = 0;
+
+        int i = 0;
+        int j = 0;
+        //Best example to understand the use of count variable - {2, 1, 2, 1}, output : 6
+        while(j < n) {
+
+            if(nums[j] % 2 != 0) { //ODD
+                oddCount++;
+                count    = 0; //We need to reset this to avoid over counting. Example : [1,1,2,1,1], k = 3
+            }
+
+            while(oddCount == k) {
+                count++;
+
+                if(i < n && nums[i] % 2 == 1) { //ODD
+                    oddCount--;
+                }
+                i++;
+            }
+
+            result += count;
+            j++;
+        }
+
+        return result;
+    }
