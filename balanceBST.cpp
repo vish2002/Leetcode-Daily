fee1e71f605ff10f40d951a00e7df91d8be7ce8a
Vishal Kurve <vishalkurve7@gmail.com>
2024-06-26 18:58:37 +0530

    Update balanceBST.cpp
    https://leetcode.com/problems/balance-a-binary-search-tree/description/?envType=daily-question&envId=2024-06-26

--- balanceBST.cpp
+++ balanceBST.cpp
@@ -2,6 +2,7 @@
 //Convert BST To Balanced BST
 // Leetcode : Medium 26-06-24
 
+// Divide and conquer Approach 
 void inorder(TreeNode* root,vector<int> &ans)
     {
         if(!root)return;
@@ -23,4 +24,30 @@ void inorder(TreeNode* root,vector<int> &ans)
         vector<int> ans;
         inorder(root,ans);
         return balancedBST(ans);
+    }
+
+// Another method using Binary Search 
+    void inorder(TreeNode* root,vector<int> &ans)
+    {
+        if(!root)return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    TreeNode*balancedBST(vector<int> ans,int low,int high)
+    {
+        if(low>high)return nullptr;
+        
+        int mid=low+(high-low)/2;
+        TreeNode*root=new TreeNode(ans[mid]);
+        root->left=balancedBST(ans,low,mid-1);
+        root->right=balancedBST(ans,mid+1,high);
+        return root;
+
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root,ans);
+        int n=ans.size()-1;
+        return balancedBST(ans,0,n);
     }
