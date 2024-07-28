0808dfcbac4c333811c8195d8c96e1cb87b1e703
Vishal Kurve <vishalkurve7@gmail.com>
2024-07-28 20:28:49 +0530

    Update 2045. Second Minimum Time to Reach Destination.cpp

--- 2045. Second Minimum Time to Reach Destination.cpp
+++ 2045. Second Minimum Time to Reach Destination.cpp
@@ -1,6 +1,7 @@
 // 2045. Second Minimum Time to Reach Destination
 // LeetCode:Hard 28-07-2024
 
+// Dijkstra
 class Solution {
 public:
 #define P pair<int,int>
@@ -50,3 +51,48 @@ public:
         return -1;
     }
 };
+// BFS
+#define P pair<int,int>
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        unordered_map<int,vector<int>> adj;
+        for(auto &edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> d1(n+1,INT_MAX);
+        vector<int> d2(n+1,INT_MAX);
+        queue<P>q;
+        q.push({1,1}); //{Node,Freq}
+        d1[1]=0;
+        while(!q.empty())
+        {
+            int freq=q.front().second;
+            int Node=q.front().first;
+            q.pop();
+            int timepassed =(freq == 1)?d1[Node]:d2[Node];
+            if(Node  == n && d2[n]!=INT_MAX)
+            return d2[n];
+
+            int div = timepassed/change;
+            if(div % 2 == 1)
+            {
+                timepassed = change*(div+1);
+            } 
+            for(auto &ngbr : adj[Node])
+            {
+                if(d1[ngbr]==INT_MAX)
+                {
+                    d1[ngbr]=timepassed+time;
+                    q.push({ngbr,1});
+                }else if(d2[ngbr]==INT_MAX && d1[ngbr]!=timepassed+time)
+                {
+                    d2[ngbr]=timepassed+time;
+                    q.push({ngbr,2});
+                }
+            }
+        }
+        return -1;
+    }
