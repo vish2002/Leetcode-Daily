2c4a531436bf7cd12f7c481f24e4f4f2f1ef55ca
Vishal Kurve <vishalkurve7@gmail.com>
2024-07-30 21:58:45 +0530

    Update 1653. Minimum Deletions to Make String Balanced.cpp

--- 1653. Minimum Deletions to Make String Balanced.cpp
+++ 1653. Minimum Deletions to Make String Balanced.cpp
@@ -17,3 +17,28 @@ int minimumDeletions(string s) {
         }
         return count;
     }
+
+// Approach 2:Precomputing left_b count and right_a count
+int minimumDeletions(string s) {
+        int n=s.length();
+        vector<int> left_b(n,0);
+        vector<int> right_a(n,0);
+        int countb=0;
+        for(int i=0;i<n;i++)
+        {
+            left_b[i]=countb;
+            if(s[i]=='b')countb++;
+        }
+        int counta=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            right_a[i]=counta;
+            if(s[i]=='a')counta++;
+        }
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            mini=min(mini,left_b[i]+right_a[i]);
+        }
+        return mini;
+    }
