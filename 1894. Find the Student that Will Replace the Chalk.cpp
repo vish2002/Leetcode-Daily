af4f32903e4676b511d6d76ff0443f807cd9efc2
Vishal Kurve <vishalkurve7@gmail.com>
2024-09-02 18:21:53 +0530

    Create 1894. Find the Student that Will Replace the Chalk.cpp
    https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/description/?envType=daily-question&envId=2024-09-02

---
+++ 1894. Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,23 @@
+// 1894. Find the Student that Will Replace the Chalk
+// Leetcode : Medium 02-09-2024
+
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int sum = 0;
+        for (int i = 0; i < chalk.size(); i++) {
+            sum += chalk[i];
+            if (sum > k) {
+                break;
+            }
+        }
+        k = k % sum;
+        for (int i = 0; i < chalk.size(); i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k = k - chalk[i];
+        }
+        return 0;
+    }
+};
