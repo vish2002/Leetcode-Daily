100fdf16190d5a82b8d0684ca0005712a5ed48ce
Vishal Kurve <vishalkurve7@gmail.com>
2024-09-05 11:40:12 +0530

    Create 2028. Find Missing Observations.cpp
    https://leetcode.com/problems/find-missing-observations/?envType=daily-question&envId=2024-09-05

---
+++ 2028. Find Missing Observations.cpp
@@ -0,0 +1,14 @@
+// 2028. Find Missing Observations
+// Leetcode : Medium 05-09-2024
+
+vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int size=rolls.size(),sum=accumulate(rolls.begin(),rolls.end(),0),missingSum=0;
+         missingSum=mean*(n+size)-sum;
+        if(missingSum<n || missingSum>6*n)
+            return {};
+         int rem=missingSum%n;
+         vector<int>ans(n,missingSum/n);
+        for(int i=0;i<rem;i++)
+            ans[i]+=1;
+        return ans;   
+    }
