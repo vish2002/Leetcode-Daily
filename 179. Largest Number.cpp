3d7827b10373a6bdea8dcf8d54173a905473f029
Vishal Kurve <vishalkurve7@gmail.com>
2024-09-18 20:43:02 +0530

    Create 179. Largest Number.cpp
    https://leetcode.com/problems/largest-number/description/?envType=daily-question&envId=2024-09-18

---
+++ 179. Largest Number.cpp
@@ -0,0 +1,18 @@
+// 179. Largest Number
+// Leetcode : Medium 18-09-2024
+
+class Solution {
+public:
+    string largestNumber(vector<int> &num) {
+        vector<string> arr;
+        for(auto i:num)
+            arr.push_back(to_string(i));
+        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
+        string res;
+        for(auto s:arr)
+            res+=s;
+        while(res[0]=='0' && res.length()>1)
+            res.erase(0,1);
+        return  res;
+    }
+};
